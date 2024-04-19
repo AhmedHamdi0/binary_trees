1c72da72d92f16e29778ac8f5e4e417363f7cc3d
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-19 19:12:12 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,35 +1,40 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the balance factor.
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the balance factor
  *
- * Return: If tree is NULL, return 0, else return balance factor.
+ * Return: Balance factor of the binary tree, 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int left_height, right_height;
 
-	return (0);
+	if (tree == NULL)
+		return (0);
+
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
+
+	return (left_height - right_height);
 }
 
+
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * Return: Height of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t left_height, right_height;
+	size_t left_height, right_height;
 
-		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((left_height > right_height) ? left_height : right_height);
-	}
+	if (tree == NULL)
+		return (0);
 
-	return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
